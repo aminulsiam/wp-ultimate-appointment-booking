2f189fefdca5c1a73ac7c91052f30115f4b4a57e
aminulsiam <aminulhossain90@gmail.com>
2020-03-13 03:46:24 -0700

    add extra service for appointment (only js code)

--- admin/class-ultapp-plugin-admin.php
+++ admin/class-ultapp-plugin-admin.php
@@ -27,12 +27,115 @@ class Ultapp_Admin {
 		add_action( 'add_meta_boxes', [ $this, 'add_meta_boxes' ] );
 	}
 
+
+	/**
+	 *
+	 */
+	public function ultapp_time_extra_services() {
+		?>
+
+        <form action="" method="post">
+            <div class="extra_service_section">
+
+                <div class="service_price">
+                    <label for="">Add Your Extra Services : </label>
+                    <input type="text" name="ex_service_name" class="ex_service_name" placeholder="service name "/>
+                    <input type="text" name="ex_service_price" class="ex_service_price" placeholder="service price"/>
+                    <input type="text" name="ex_service_time" class="ex_service_time" placeholder="service time"/>
+
+                    <a href="#" class="button ex_service_price_add">
+                        <span class="dashicons dashicons-plus-alt"
+                              style="margin-top:3px;color: green;"></span>Add
+                    </a>
+
+                </div>
+
+                <div class="service_pricing_table">
+                    <table>
+                        <tr>
+                            <th>Extra Services Name</th>
+                            <th>Extra Services Price</th>
+                            <th>Extra Services Time</th>
+                            <th>Action</th>
+                        </tr>
+
+
+
+
+                        <?php
+						$post_id = isset( $_GET['post'] ) ? $_GET['post'] : "";
+
+						$get_service_pricing = maybe_unserialize( get_post_meta( $post_id, 'service_pricing', true ) );
+
+						$last_count = isset( $get_service_pricing['service_price_last_count'] ) ? $get_service_pricing['service_price_last_count'] : 0;
+
+						if ( ! is_array( $get_service_pricing ) ) {
+							$get_service_pricing = array();
+						}
+
+
+						foreach ( $get_service_pricing as $key => $service_pricing ) {
+							if ( "service_price_last_count" != $key ) {
+								?>
+                                <tr>
+                                    <td>
+										<?php echo $service_pricing['service_title']; ?>
+                                        <input type="hidden"
+                                               name="service_pricing[<?php echo $key; ?>][service_title]"
+                                               value="<?php echo $service_pricing['service_title']; ?>">
+                                    </td>
+
+                                    <td>
+										<?php echo $service_pricing['service_price']; ?>
+
+                                        <input type="hidden" class="service_price"
+                                               name="service_pricing[<?php echo $key; ?>][service_price]"
+                                               value="<?php echo $service_pricing['service_price']; ?>">
+                                    </td>
+
+                                    <td>
+
+                                        <input type="number" min="1"
+                                               value="<?php echo $service_pricing['qty']; ?>"
+                                               name="service_pricing[<?php echo $key; ?>][qty]"
+                                               class="service_qty qty">
+
+                                    </td>
+
+                                    <td>
+                                        <a href="" class="button service_price_remove"><span
+                                                    class="dashicons dashicons-trash"
+                                                    style="margin-top: 3px;color: red;"></span>Remove</a>
+                                    </td>
+                                </tr>
+
+								<?php
+							}
+						} ?>
+                    </table>
+
+<!--                    <input type="hidden" name="submit_service_pricing">-->
+					<?php //wp_nonce_field( 'service_prices_metabox_nonce', 'service_prices_nonce' ); ?>
+
+                    <input type="hidden" name="service_pricing[service_price_last_count]"
+                           class="service_price_last_count" value="<?php echo $last_count; ?>">
+                </div>
+
+            </div>
+        </form>
+
+		<?php
+	}//end method ultapp_time_extra_services
+
+
 	/**
 	 * Display time settings input fields.
 	 */
 	public function ultapp_time_settings_in_meta() {
 
-	}//
+		//time settings code is written here
+
+	}//end method ultapp_time_settings_in_meta
 
 	/**
 	 * Add custom meta boxes.
@@ -40,14 +143,25 @@ class Ultapp_Admin {
 	public function add_meta_boxes() {
 
 		add_meta_box(
-			'ultapp_time_metabox',
+			'ultapp_time_settings_metabox',
 			'Wp unlimited appointment time settings',
 			[ $this, 'ultapp_time_settings_in_meta' ],
 			[ 'appointment' ]
 		);
 
+		add_meta_box(
+			'ultapp_extra_service_metabox',
+			'Wp unlimited appointment extra services with appointment',
+			[ $this, 'ultapp_time_extra_services' ],
+			[ 'appointment' ]
+		);
+
+
 	}//end method add_meta_boxes
 
+	/**
+	 * Load all styles
+	 */
 	public function enqueue_styles() {
 		wp_enqueue_style( 'mage-jquery-ui-style', ULTAPP_PLUGIN_URL . 'admin/css/jquery-ui.css', array() );
 		wp_enqueue_style( 'pickplugins-options-framework', ULTAPP_PLUGIN_URL . 'admin/assets/css/pickplugins-options-framework.css' );
@@ -58,6 +172,9 @@ class Ultapp_Admin {
 		wp_enqueue_style( 'mage-admin-css', ULTAPP_PLUGIN_URL . 'admin/css/mage-plugin-admin.css', array(), time(), 'all' );
 	}
 
+	/**
+	 * Load all scripts
+	 */
 	public function enqueue_scripts() {
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'jquery-ui-core' );
--- admin/js/ultapp-plugin-admin.js
+++ admin/js/ultapp-plugin-admin.js
@@ -1,5 +1,51 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+
+    //===============================================
+
+    // Extra Service add by selected services
+
+    //===============================================
+    $(".ex_service_price_add").on("click", function (e) {
 
 
+        e.preventDefault();
 
-  }); 
+        var $post_id = $(".selected_services").val();
+
+        var ex_service_name = $('.ex_service_name').val();
+        var ex_service_price = $('.ex_service_price').val();
+        var ex_service_time = $('.ex_service_time').val();
+
+        var fields = '<tr>' +
+            '<td><input type="hidden" required value="' + ex_service_name + '" name="ex_service_name" class="ex_service_name" />' + ex_service_name + '</td>' +
+            '<td><input type="hidden" value="' + ex_service_price + '" name="ex_service_price" class="ex_service_price" />' + ex_service_price + '</td>' +
+            '<td><input type="hidden" value="' + ex_service_time + '" name="ex_service_time" class="ex_service_time" />' + ex_service_time + '</td>' +
+            '<td><a href="" class="ex_service_remove button"><span class="dashicons dashicons-trash" style="margin-top: 3px;color: red;"></span> Remove </a></td>' +
+            '</tr>';
+
+        $('.service_pricing_table table').append(fields);
+
+
+        var $last_count = $('.service_price_last_count');
+        var $last_count_val = parseInt($last_count.val());
+
+        $last_count_val++;
+
+        $last_count.val($last_count_val);
+
+    });
+
+    //================
+
+    // Remove extra service section specific row
+
+    //================
+    $(".extra_service_section").on("click", ".ex_service_remove", function (e) {
+
+        e.preventDefault();
+
+        $(this).closest('tr').remove();
+    });
+
+
+});
